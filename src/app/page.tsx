2ec1c26de21817509d94da32bf72125f600c095b
Kaz-gif <thechurroman360@gmail.com>
2025-05-11 17:15:51 -0600

    Modificaciones post-revision

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,62 +4,50 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main>
-        <header>
-          <h1>Residencial Los Robles</h1>
-          <p>Bienvenido al portal de gestión vecinal</p>
-        </header>
+        <body>
+          <header>
+            <h1><svg xmlns="http://www.w3.org/2000/svg" height="32" width="28" viewBox="0 15 448 430"><path fill="#ffffff" d="M210.6 5.9L62 169.4c-3.9 4.2-6 9.8-6 15.5C56 197.7 66.3 208 79.1 208l24.9 0L30.6 281.4c-4.2 4.2-6.6 10-6.6 16C24 309.9 34.1 320 46.6 320L80 320 5.4 409.5C1.9 413.7 0 419 0 424.5c0 13 10.5 23.5 23.5 23.5L192 448l0 32c0 17.7 14.3 32 32 32s32-14.3 32-32l0-32 168.5 0c13 0 23.5-10.5 23.5-23.5c0-5.5-1.9-10.8-5.4-15L368 320l33.4 0c12.5 0 22.6-10.1 22.6-22.6c0-6-2.4-11.8-6.6-16L344 208l24.9 0c12.7 0 23.1-10.3 23.1-23.1c0-5.7-2.1-11.3-6-15.5L237.4 5.9C234 2.1 229.1 0 224 0s-10 2.1-13.4 5.9z"/></svg> Residencial Los Robles</h1>
+            <p>Bienvenido al portal de gestión vecinal</p>
+          </header>
 
-        <nav>
-          <a href="#pagos">Gestión de Pagos</a>
-          <a href="#notificaciones">Notificaciones</a>
-          <a href="#incidencias">Incidencias</a>
-          <a href="#reportes">Reportes</a>
-        </nav>
+          <nav>
+            <a href="#pagos"><svg xmlns="http://www.w3.org/2000/svg" height="14" width="15.75" viewBox="100 -50 576 512"><path fill="#ffffff" d="M64 32C28.7 32 0 60.7 0 96l0 32 576 0 0-32c0-35.3-28.7-64-64-64L64 32zM576 224L0 224 0 416c0 35.3 28.7 64 64 64l448 0c35.3 0 64-28.7 64-64l0-192zM112 352l64 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-64 0c-8.8 0-16-7.2-16-16s7.2-16 16-16zm112 16c0-8.8 7.2-16 16-16l128 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-128 0c-8.8 0-16-7.2-16-16z"/></svg>Pagos</a>
+            <a href="#notificaciones"><svg xmlns="http://www.w3.org/2000/svg" height="14" width="12.25" viewBox="0 -50 448 512"><path fill="#ffffff" d="M224 0c-17.7 0-32 14.3-32 32l0 19.2C119 66 64 130.6 64 208l0 18.8c0 47-17.3 92.4-48.5 127.6l-7.4 8.3c-8.4 9.4-10.4 22.9-5.3 34.4S19.4 416 32 416l384 0c12.6 0 24-7.4 29.2-18.9s3.1-25-5.3-34.4l-7.4-8.3C401.3 319.2 384 273.9 384 226.8l0-18.8c0-77.4-55-142-128-156.8L256 32c0-17.7-14.3-32-32-32zm45.3 493.3c12-12 18.7-28.3 18.7-45.3l-64 0-64 0c0 17 6.7 33.3 18.7 45.3s28.3 18.7 45.3 18.7s33.3-6.7 45.3-18.7z"/></svg> Notificaciones</a>
+            <a href="#incidencias"><svg xmlns="http://www.w3.org/2000/svg" height="14" width="14" viewBox="0 -50 512 512"><path fill="#ffffff" d="M256 32c14.2 0 27.3 7.5 34.5 19.8l216 368c7.3 12.4 7.3 27.7 .2 40.1S486.3 480 472 480L40 480c-14.3 0-27.6-7.7-34.7-20.1s-7-27.8 .2-40.1l216-368C228.7 39.5 241.8 32 256 32zm0 128c-13.3 0-24 10.7-24 24l0 112c0 13.3 10.7 24 24 24s24-10.7 24-24l0-112c0-13.3-10.7-24-24-24zm32 224a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z"/></svg> Incidencias</a>
+            <a href="#reportes"><svg xmlns="http://www.w3.org/2000/svg" height="14" width="14" viewBox="0 -50 512 512"><path fill="#ffffff" d="M32 32c17.7 0 32 14.3 32 32l0 336c0 8.8 7.2 16 16 16l400 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L80 480c-44.2 0-80-35.8-80-80L0 64C0 46.3 14.3 32 32 32zm96 96c0-17.7 14.3-32 32-32l192 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-192 0c-17.7 0-32-14.3-32-32zm32 64l128 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-128 0c-17.7 0-32-14.3-32-32s14.3-32 32-32zm0 96l256 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-256 0c-17.7 0-32-14.3-32-32s14.3-32 32-32z"/></svg> Reportes</a>
+          </nav>
 
-        <div className="container">
-          <div id="pagos" className="card">
-            <h3>Gestión de Pagos</h3>
-            <p>
-              Consulta tu historial de pagos y realiza nuevas contribuciones
-              fácilmente.
-            </p>
-            <a href="#" className="btn">
-              Ver historial de pagos
-            </a>
-            <a href="#" className="btn">
-              Realizar nuevo pago
-            </a>
+          <div className="container">
+            <div id="pagos" className="card">
+              <h3><svg xmlns="http://www.w3.org/2000/svg" height="20" width="22.5" viewBox="50 -40 576 512"><path fill="#2f855a" d="M64 32C28.7 32 0 60.7 0 96l0 32 576 0 0-32c0-35.3-28.7-64-64-64L64 32zM576 224L0 224 0 416c0 35.3 28.7 64 64 64l448 0c35.3 0 64-28.7 64-64l0-192zM112 352l64 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-64 0c-8.8 0-16-7.2-16-16s7.2-16 16-16zm112 16c0-8.8 7.2-16 16-16l128 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-128 0c-8.8 0-16-7.2-16-16z"/></svg>Gestión de Pagos</h3>
+              <p>Consulta tu historial de pagos y realiza nuevas contribuciones fácilmente.</p>
+              <a href="#" className="btn">Ver historial de pagos</a>
+              <a href="#" className="btn">Realizar nuevo pago</a>
+            </div>
+
+            <div id="notificaciones" className="card">
+              <h3><svg xmlns="http://www.w3.org/2000/svg" height="20" width="22.5" viewBox="0 -20 576 512"><path fill="#2f855a" d="M224 0c-17.7 0-32 14.3-32 32l0 19.2C119 66 64 130.6 64 208l0 18.8c0 47-17.3 92.4-48.5 127.6l-7.4 8.3c-8.4 9.4-10.4 22.9-5.3 34.4S19.4 416 32 416l384 0c12.6 0 24-7.4 29.2-18.9s3.1-25-5.3-34.4l-7.4-8.3C401.3 319.2 384 273.9 384 226.8l0-18.8c0-77.4-55-142-128-156.8L256 32c0-17.7-14.3-32-32-32zm45.3 493.3c12-12 18.7-28.3 18.7-45.3l-64 0-64 0c0 17 6.7 33.3 18.7 45.3s28.3 18.7 45.3 18.7s33.3-6.7 45.3-18.7z"/></svg> Notificaciones</h3>
+              <p>Últimos avisos sobre eventos, servicios y actualizaciones importantes:</p>
+              <ul>
+                <li><svg xmlns="http://www.w3.org/2000/svg" height="14" width="14" viewBox="0 0 512 512"><path fill="#000000" d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zm0-384c13.3 0 24 10.7 24 24l0 112c0 13.3-10.7 24-24 24s-24-10.7-24-24l0-112c0-13.3 10.7-24 24-24zM224 352a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z"/></svg>Corte de agua programado - 10 de noviembre</li>
+                <li><svg xmlns="http://www.w3.org/2000/svg" height="14" width="17.5" viewBox="0 0 670 512"><path fill="#000000" d="M144 0a80 80 0 1 1 0 160A80 80 0 1 1 144 0zM512 0a80 80 0 1 1 0 160A80 80 0 1 1 512 0zM0 298.7C0 239.8 47.8 192 106.7 192l42.7 0c15.9 0 31 3.5 44.6 9.7c-1.3 7.2-1.9 14.7-1.9 22.3c0 38.2 16.8 72.5 43.3 96c-.2 0-.4 0-.7 0L21.3 320C9.6 320 0 310.4 0 298.7zM405.3 320c-.2 0-.4 0-.7 0c26.6-23.5 43.3-57.8 43.3-96c0-7.6-.7-15-1.9-22.3c13.6-6.3 28.7-9.7 44.6-9.7l42.7 0C592.2 192 640 239.8 640 298.7c0 11.8-9.6 21.3-21.3 21.3l-213.3 0zM224 224a96 96 0 1 1 192 0 96 96 0 1 1 -192 0zM128 485.3C128 411.7 187.7 352 261.3 352l117.3 0C452.3 352 512 411.7 512 485.3c0 14.7-11.9 26.7-26.7 26.7l-330.7 0c-14.7 0-26.7-11.9-26.7-26.7z"/></svg>Asamblea general - 15 de noviembre a las 19:00 hrs</li>
+                <li><svg xmlns="http://www.w3.org/2000/svg" height="14" width="14" viewBox="0 0 512 512"><path fill="#000000" d="M210.6 5.9L62 169.4c-3.9 4.2-6 9.8-6 15.5C56 197.7 66.3 208 79.1 208l24.9 0L30.6 281.4c-4.2 4.2-6.6 10-6.6 16C24 309.9 34.1 320 46.6 320L80 320 5.4 409.5C1.9 413.7 0 419 0 424.5c0 13 10.5 23.5 23.5 23.5L192 448l0 32c0 17.7 14.3 32 32 32s32-14.3 32-32l0-32 168.5 0c13 0 23.5-10.5 23.5-23.5c0-5.5-1.9-10.8-5.4-15L368 320l33.4 0c12.5 0 22.6-10.1 22.6-22.6c0-6-2.4-11.8-6.6-16L344 208l24.9 0c12.7 0 23.1-10.3 23.1-23.1c0-5.7-2.1-11.3-6-15.5L237.4 5.9C234 2.1 229.1 0 224 0s-10 2.1-13.4 5.9z"/></svg> Nuevo parque en construcción en la zona norte</li>
+              </ul>
+            </div>
+
+            <div id="incidencias" className="card">
+              <h3><svg xmlns="http://www.w3.org/2000/svg" height="20" width="22.5" viewBox="0 -20 576 512"><path fill="#2f855a" d="M256 32c14.2 0 27.3 7.5 34.5 19.8l216 368c7.3 12.4 7.3 27.7 .2 40.1S486.3 480 472 480L40 480c-14.3 0-27.6-7.7-34.7-20.1s-7-27.8 .2-40.1l216-368C228.7 39.5 241.8 32 256 32zm0 128c-13.3 0-24 10.7-24 24l0 112c0 13.3 10.7 24 24 24s24-10.7 24-24l0-112c0-13.3-10.7-24-24-24zm32 224a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z"/></svg> Reportar Incidencia</h3>
+              <p>¿Algún problema en la colonia? Repórtalo aquí.</p>
+              <a href="#" className="btn">Nueva incidencia</a>
+            </div>
+
+            <div id="reportes" className="card">
+              <h3><svg xmlns="http://www.w3.org/2000/svg" height="20" width="22.5" viewBox="0 -20 576 512"><path fill="#2f855a" d="M32 32c17.7 0 32 14.3 32 32l0 336c0 8.8 7.2 16 16 16l400 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L80 480c-44.2 0-80-35.8-80-80L0 64C0 46.3 14.3 32 32 32zm96 96c0-17.7 14.3-32 32-32l192 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-192 0c-17.7 0-32-14.3-32-32zm32 64l128 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-128 0c-17.7 0-32-14.3-32-32s14.3-32 32-32zm0 96l256 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-256 0c-17.7 0-32-14.3-32-32s14.3-32 32-32z"/></svg> Reportes</h3>
+              <p>Visualiza reportes financieros y de participación vecinal.</p>
+              <a href="#" className="btn">Ver reportes</a>
+            </div>
           </div>
-
-          <div id="notificaciones" className="card">
-            <h3>Notificaciones</h3>
-            <p>
-              Últimos avisos sobre eventos, servicios y actualizaciones
-              importantes:
-            </p>
-            <ul>
-              <li>🔔 Corte de agua programado - 10 de noviembre</li>
-              <li>🔔 Asamblea general - 15 de noviembre a las 19:00 hrs</li>
-              <li>🔔 Nuevo parque en construcción en la zona norte</li>
-            </ul>
-          </div>
-
-          <div id="incidencias" className="card">
-            <h3>Reportar Incidencia</h3>
-            <p>¿Algún problema en la colonia? Repórtalo aquí.</p>
-            <a href="#" className="btn">
-              Nueva incidencia
-            </a>
-          </div>
-
-          <div id="reportes" className="card">
-            <h3>Reportes</h3>
-            <p>Visualiza reportes financieros y de participación vecinal.</p>
-            <a href="#" className="btn">
-              Ver reportes
-            </a>
-          </div>
-        </div>
+        </body>
       </main>
     </div>
   );
